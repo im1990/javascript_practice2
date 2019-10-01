dec929efa9b6883e7cd99d8f4b104b6edd234bfb
Miki Ishibashi <52851340+im1990@users.noreply.github.com>
2019-10-01 23:01:22 +0900

    【更新】modify.js
    下記修正いたしました。
    ・配列の変数（let => const）
    ・コンストラクタ関数名（Obj => TodoObj）
    ・オブジェクトの変数（let => const）
    ・配列の表示を非表示＝＞再表示させる
    ・onclickを全てaddEentListenerに変更
    ・ソート時に、配列の中身を非表示にするのではなく、再表示させる式に変更しました。
    
    問題点
    ・ソートをかけると、「完了」となっていたアイテムも「作業中」の表示に変わってしまいます。
    　ただ、配列のデータにはきちんとしたtrue/falseの値が格納されています。
    　色々と試しましたが修正できず、アドバイス頂けますと幸いです。

--- modify.js
+++ modify.js
@@ -1,49 +1,49 @@
 const lists = document.querySelector('#list');
 const input = document.querySelector('#input');
 
-let todoArray = []; // [{obj1}, {obj2}　...]
+const todoArray = []; // [{obj1}, {obj2}　...]
 
-const Obj = function (id, todo, status) {
+const TaskObj = function (id, todo, status) {
     this.id = id;
     this.todo = todo;
     this.status = status;
 }
 
-/********************
-* 　リスト追加機能　　*
-********************/
-function addList() {
+/**************************
+*  　トグルボタン追加機能　　*
+***************************/
+const createToggleBtn = () => {
 
-    let status = false;
-    let text = input.value;
-    let ID;
-
-    // IDをつける
-    if(todoArray.length > 0) {
-        ID = todoArray.length;
-    } else {
-        ID = 0;
-    }
-
-    // 新しいオブジェクトを生成して配列に入れる
-    let newObj = new Obj(ID , text, status);
-    todoArray.push(newObj);
-
-    // 作業中/完了ボタンを表示 
     const toggleBtn = document.createElement('button');
-    toggleBtn.textContent = '作業中';
+    // 作業中/完了ボタンを表示 
+    
+    todoArray.forEach( index => {
+        if(!index.status) {
+            toggleBtn.textContent = '作業中';
+        } else {
+            toggleBtn.textContent = '完了';
+        }
+    });
+    
     // 作業中/完了ボタンのイベント
     toggleBtn.addEventListener('click', e => {
         let currentID = parseInt(e.target.parentElement.parentElement.firstElementChild.textContent);
-        if (todoArray[currentID].status === false) {
+        if (!todoArray[currentID].status) {
             e.target.textContent = '完了';
             todoArray[currentID].status = true;
         } else {
             e.target.textContent = '作業中';
             todoArray[currentID].status = false;
-        }
+        }        
+        displayTodos(todoArray);
     });
+    return toggleBtn;
+}
 
+/*************************
+*  　削除ボタン追加機能　　*
+**************************/
+const createDeleteBtn = () => {
     // 削除ボタンを表示
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = '削除'
@@ -51,33 +51,85 @@ function addList() {
     deleteBtn.addEventListener('click', e => {
         let currentID = parseInt(e.target.parentElement.parentElement.firstElementChild.textContent);
         todoArray.splice(currentID, 1);
-        tr.style.display = 'none';
+        //配列の再生成
+        createTodos(todoArray);
     });
+    return deleteBtn;
+}
 
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
+/********************
+* 　リスト作成機能　　*
+********************/
+const createTodos = array => {
+        lists.textContent = '';
+        array.forEach((index, value) => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
 
-    // td1 : ID表示
-    td1.innerHTML = ID;
+        // td1 : ID表示
+        td1.innerHTML = value;
 
-    // td2 : コメント表示
-    td2.innerHTML = ` ${todoArray[ID].todo} `;
+        // td2 : コメント表示
+        td2.innerHTML = ` ${index.todo} `;
 
-    // td3 : ボタン表示
-    td3.appendChild(toggleBtn);
-    td3.appendChild(deleteBtn);
+        // td3 : ボタン表示
+        const toggleButton = createToggleBtn();
+        const deleteButton = createDeleteBtn();
+        td3.appendChild(toggleButton);
+        td3.appendChild(deleteButton);
 
-    // 全部ひとまとめ
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    lists.appendChild(tr);
+        // 全部ひとまとめ
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        lists.appendChild(tr);
+    });
+}
 
-    // inputの中身をリセット
+/********************
+* 　リスト表示機能　　*
+********************/
+const displayTodos = array => {
+    let status = false;
+    let text = input.value;
+    let id = 0;
+
+    if(text) {
+        // IDをつける
+        if(array.length) {
+            id = array.length;
+        } else {
+            id = 0;
+        }
+
+        // 表示を初期化
+        lists.textContent = '';
+
+        // 新しいオブジェクトを生成して配列に入れる
+        const newTaskObj = new TaskObj(id, text, status);
+        array.push(newTaskObj);
+
+        array.forEach((index, value) => {
+            createTodos(array);
+        });
+    }    
+}
+
+/********************
+* 　リスト追加機能　　*
+********************/
+const addList = function() {
+
+    displayTodos(todoArray);
+
     input.value = '';
 }
+
+// 「追加」ボタンをクリックするとリスト追加
+const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', addList);
 
 // クリックすればリスト追加
 input.addEventListener('keypress', e => {
@@ -90,50 +142,51 @@ input.addEventListener('keypress', e => {
 * 　　ソート機能　　　*
 ********************/
 // 非表示アイテム格納用配列
+// ここはconstにするとエラー：Assignment to constant variable
 let sortArray = [];
 
 // ラジオボタン「すべて」ですべて表示に戻す
-function everything() {
+const everything = function() {
     sortArray = []; // 配列の中をクリア
 
-    // すべて表示する
-    for(let i = 0; i < todoArray.length; i++) {
-        const listChildren = lists.children[i];
-        listChildren.style.display = 'table-row';
-    }
+    createTodos(todoArray);
+    displayTodos(todoArray);
 }
+const everyButton = document.getElementById('everyButton');
+everyButton.addEventListener('click', everything);
+
 
 // ラジオボタン「作業中」によるソート
-function working() {
+const workButton = document.getElementById('workButton');
+workButton.addEventListener('click', () => {
     // ソート用配列の初期化
     everything();
 
     // 「作業中」以外の要素（完了ボタン）のみ抽出
-    const inactiveTodo = todoArray.filter(x => x.status); // status:true のものだけ抽出
+    const inactiveTodo = todoArray.filter(x => !x.status); // status:true のものだけ抽出
     sortArray.push(inactiveTodo);
 
-    // 「作業中」以外の要素（完了ボタン）を非表示にする 
-    sort();
-}
+    // 「作業中」のみ表示させる
+    if(inactiveTodo) {
+        createTodos(sortArray[0]);
+        displayTodos(sortArray[0]);
+    }
+});
 
-// ラジオボタン「完了」によるソート
-function complete() {
+
+// ラジオボタン「完了」によるソート 
+const completeButton = document.getElementById('completeButton');
+completeButton.addEventListener('click', () => {
     // ソート用配列の初期化
     everything();
 
     // 「完了」以外の要素（作業中ボタン）のみ抽出
-    const inactiveTodo = todoArray.filter(x => !x.status);
+    const inactiveTodo = todoArray.filter(x => x.status);
     sortArray.push(inactiveTodo);
 
-    // 「完了」以外の要素（作業中ボタン）を非表示にする 
-    sort();
-}
-
-// 非表示にするアイテムを隠す
-function sort() {
-    for(let i = 0; i < sortArray[0].length; i++) {
-        let hideID = sortArray[0][i].id;
-        let hideElement = lists.children[hideID]
-        hideElement.style.display = 'none';
-    } 
-}
+    // 「完了」のみ表示させる
+    if(inactiveTodo) {
+        createTodos(sortArray[0]);
+        displayTodos(sortArray[0]);
+    }
+});
